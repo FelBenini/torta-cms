5b4dd0d0635c21b4b111bae9fc0f6af550cd7a88
felbenini <felbenini@gmail.com>
2023-06-16 23:39:56 -0300

    Now the getSizeOfDatabase return the size of all collections

--- lib/mongodb/controllers/mongoController.ts
+++ lib/mongodb/controllers/mongoController.ts
@@ -1,13 +1,21 @@
 import dbConnect from "..";
 import User from "../models/User"
+import Post from "../models/Post";
+import Category from "../models/Category";
 
 export default class mongoController {
   public static getSizeOfDatabase = async () => {
     await dbConnect()
     const userCollection = await User.aggregate([{$collStats: {storageStats: {}}}]).exec()
     const sizeOfUserCollection = userCollection[0].storageStats.size;
+    const postCollection = await Post.aggregate([{$collStats: {storageStats: {}}}]).exec()
+    const sizeOfPostCollection = postCollection[0].storageStats.size;
+    const categoryCollection = await Category.aggregate([{$collStats: {storageStats: {}}}]).exec()
+    const sizeOfCategoryCollection = categoryCollection[0].storageStats.size;
     return {
-      userCollection: sizeOfUserCollection
+      userCollection: sizeOfUserCollection,
+      postCollection: sizeOfPostCollection,
+      categoryCollection: sizeOfCategoryCollection
     }
   }
 }
