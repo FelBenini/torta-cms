ca9569883d86d2cbce2563578bc2f89f2d12488c
felbenini <felbenini@gmail.com>
2023-06-18 18:14:55 -0300

    Added pagination component

---
+++ components/Pagination/index.tsx
@@ -0,0 +1,23 @@
+'use client'
+import React, {useState} from 'react'
+import Pagination from '@mui/material/Pagination';
+import { useRouter } from 'next/navigation'
+import Stack from '@mui/material/Stack';
+import NProgress from "nprogress";
+
+const PaginationComponent = ({num, pageNum}: {num: number, pageNum: string}) => {
+  const router = useRouter()
+  const [page, setPage] = useState(parseInt(pageNum))
+  const handleChange = (e: React.ChangeEvent<unknown>, value: number) => {
+    setPage(value)
+    router.push(`?page=${value}`)
+    NProgress.start();
+  }
+  return (
+    <Stack height={50} alignItems="center" justifyContent="center">
+      <Pagination sx={{margin: '0 auto'}} count={num} defaultPage={page} page={page} onChange={handleChange}/>
+    </Stack>
+  )
+}
+
+export default PaginationComponent
