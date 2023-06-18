65e02007afb8daa7a10fb528c0e6593ab5858946
felbenini <felbenini@gmail.com>
2023-06-18 14:13:03 -0300

    Added write post page and fixed the redirect of the /dashboard/newpost

--- app/dashboard/newpost/page.tsx
+++ app/dashboard/newpost/page.tsx
@@ -5,7 +5,7 @@ import { getServerSession } from 'next-auth';
 const NewPostPage = async () => {
   const session = await getServerSession();
   const res = await postController.createPost(session?.user?.name as string)
-  redirect(`/post/${res}`)
+  redirect(`/dashboard/post/${res}`)
 }
 
 export default NewPostPage
