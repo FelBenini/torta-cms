8174178f749796ac684cdf13a9121c79f877b990
felbenini <felbenini@gmail.com>
2023-06-21 15:05:52 -0300

    Added a drawer on post page that will be useful later

--- components/WritePost/index.tsx
+++ components/WritePost/index.tsx
@@ -10,29 +10,49 @@ import { RiMenu3Fill } from 'react-icons/ri'
 import Stack from '@mui/material/Stack'
 import { AiOutlineSave } from 'react-icons/ai'
 import axios from 'axios'
+import Drawer from '@mui/material/Drawer';
 
 const WritePost = ({ post }: { post: Post }) => {
   const [content, setContent] = useState(post.content)
   const [title, setTitle] = useState(post.title)
+  const [showing, setShowing] = useState(false)
   const [saving, setSaving] = useState(false)
   async function handleSave() {
     setSaving(true)
-    const {status} = await axios.put(`/api/content/save-content-changes/${post._id}`, {
+    const { status } = await axios.put(`/api/content/save-content-changes/${post._id}`, {
       title: title,
       content: content
     })
     setSaving(false)
   }
-  
+
+  const toggleDrawer =
+    (open: boolean) =>
+      (event: React.KeyboardEvent | React.MouseEvent) => {
+        if (
+          event.type === 'keydown' &&
+          ((event as React.KeyboardEvent).key === 'Tab' ||
+            (event as React.KeyboardEvent).key === 'Shift')
+        ) {
+          return;
+        }
+        setShowing(open);
+      };
+
   return (
     <div className={styles.writePost}>
+      <Drawer
+        anchor='right'
+        open={showing}
+        onClose={toggleDrawer(false)}
+      ><p>dsadsadasad</p></Drawer>
       <Stack direction='row' spacing={4} justifyContent='flex-end'>
-        {saving ? 
-        <p>Saving changes...</p> :
-        <Button startIcon={<AiOutlineSave />} sx={{ textTransform: 'none' }} onClick={handleSave}>Save changes</Button>
+        {saving ?
+          <p>Saving changes...</p> :
+          <Button startIcon={<AiOutlineSave />} sx={{ textTransform: 'none' }} onClick={handleSave}>Save changes</Button>
         }
         <Button variant='contained'>Publish</Button>
-        <IconButton size='large'>
+        <IconButton size='large' onClick={toggleDrawer(true)}>
           <RiMenu3Fill />
         </IconButton>
       </Stack>
