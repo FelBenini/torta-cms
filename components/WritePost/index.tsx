1bd859e5f2ef49696b39e447cde6a0df3297bbe0
felbenini <felbenini@gmail.com>
2023-06-21 14:25:06 -0300

    Now the post page saves the title and content!!!

--- components/WritePost/index.tsx
+++ components/WritePost/index.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useEffect } from 'react'
+import React from 'react'
 import TextEditor from '../TextEditor'
 import { type Post } from '../Posts/PostCard'
 import { useState } from 'react'
@@ -9,21 +9,28 @@ import Button from '@mui/material/Button'
 import { RiMenu3Fill } from 'react-icons/ri'
 import Stack from '@mui/material/Stack'
 import { AiOutlineSave } from 'react-icons/ai'
+import axios from 'axios'
 
 const WritePost = ({ post }: { post: Post }) => {
-  useEffect(() => {
-    window.onbeforeunload = function () {
-      return 'Discard changes?';
-    };
-    window.onbeforeunload = null;
-  })
-
   const [content, setContent] = useState(post.content)
   const [title, setTitle] = useState(post.title)
+  const [saving, setSaving] = useState(false)
+  async function handleSave() {
+    setSaving(true)
+    const {status} = await axios.put(`/api/content/save-content-changes/${post._id}`, {
+      title: title,
+      content: content
+    })
+    setSaving(false)
+  }
+  
   return (
     <div className={styles.writePost}>
       <Stack direction='row' spacing={4} justifyContent='flex-end'>
-        <Button startIcon={<AiOutlineSave />} sx={{ textTransform: 'none' }}>Save changes</Button>
+        {saving ? 
+        <p>Saving changes...</p> :
+        <Button startIcon={<AiOutlineSave />} sx={{ textTransform: 'none' }} onClick={handleSave}>Save changes</Button>
+        }
         <Button variant='contained'>Publish</Button>
         <IconButton size='large'>
           <RiMenu3Fill />
