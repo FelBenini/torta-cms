89e9fef3efd42d64b97ce293d950f1e1632c8383
felbenini <felbenini@gmail.com>
2023-06-22 21:21:40 -0300

    Refactored the categories model

--- lib/mongodb/models/Category.ts
+++ lib/mongodb/models/Category.ts
@@ -1,7 +1,10 @@
 import mongoose, { model } from 'mongoose'
 
 const CategorySchema = new mongoose.Schema({
-  name: {type: String, required: true}
+  name: {type: String, required: true},
+  type: {type: String, required: true},
+  childCategories: [{type: mongoose.Schema.Types.ObjectId}],
+  mainCategory: {type: mongoose.Schema.Types.ObjectId}
 })
 
 const Category = mongoose.models.Category ||  model('Category', CategorySchema);
