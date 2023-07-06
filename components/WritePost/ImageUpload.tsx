238bc627ea0976b2526546348ccec5dde25587ef
felbenini <felbenini@gmail.com>
2023-07-05 21:38:09 -0300

    Now when changing the image from the input, it gets uploaded to the database

--- components/WritePost/ImageUpload.tsx
+++ components/WritePost/ImageUpload.tsx
@@ -2,14 +2,26 @@
 import React from 'react'
 import styles from './image.module.scss'
 import { BiUpload } from 'react-icons/bi'
+import axios from 'axios'
 
 const ImageUpload = () => {
+  const handleUpload = (e: any) => {
+    const file = e.target.files[0];
+    const formData = new FormData();
+    formData.append('file', file, file.filename);
+    axios.post('/api/upload', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      }
+    })
+  }
+
   return (
     <>
       <label className={styles.uploadFile}>
         Click here to Upload
         <BiUpload style={{ display: 'block', marginTop: '6px' }} size='1.5em'/>
-        <input type='file' name='uploadFile'/>
+        <input onChange={handleUpload} type='file' name='uploadFile'/>
       </label>
     </>
   )
