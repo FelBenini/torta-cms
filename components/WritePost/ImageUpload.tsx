0ca9406d00ee5135371c6d541dd0662bd342333f
felbenini <felbenini@gmail.com>
2023-07-05 22:23:16 -0300

    Added an image preview and a input to insert image url

--- components/WritePost/ImageUpload.tsx
+++ components/WritePost/ImageUpload.tsx
@@ -1,28 +1,36 @@
 'use client'
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './image.module.scss'
 import { BiUpload } from 'react-icons/bi'
 import axios from 'axios'
 
 const ImageUpload = () => {
-  const handleUpload = (e: any) => {
-    const file = e.target.files[0];
+  const [imgLink, setImgLink] = useState('')
+  const handleUpload = async (e: React.FormEvent<HTMLInputElement>) => {
+    const target = e.target as HTMLInputElement
+    if (!target.files) {
+      return
+    }
+    const file = target.files[0];
     const formData = new FormData();
-    formData.append('file', file, file.filename);
-    axios.post('/api/upload', formData, {
+    formData.append('file', file);
+    const {data} = await axios.post('/api/upload', formData, {
       headers: {
         'Content-Type': 'multipart/form-data',
       }
     })
+    setImgLink(data.location)
   }
 
   return (
     <>
+      <img className={styles.imgPreview} src={imgLink}/>
       <label className={styles.uploadFile}>
         Click here to Upload
         <BiUpload style={{ display: 'block', marginTop: '6px' }} size='1.5em'/>
-        <input onChange={handleUpload} type='file' name='uploadFile'/>
+        <input onChange={handleUpload} type='file' name='uploadFile'/>  
       </label>
+      <input defaultValue={imgLink} onBlur={(e) => {setImgLink(e.target.value)}}/>
     </>
   )
 }
