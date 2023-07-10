43226a81e4286c53b5745af6b6e7e6087496a8f6
felbenini <felbenini@gmail.com>
2023-07-10 16:03:46 -0300

    Changes to the api-ref page

--- app/dashboard/api-ref/page.tsx
+++ app/dashboard/api-ref/page.tsx
@@ -11,9 +11,9 @@ const ApiRefPage = () => {
   const host = referer?.replace('/dashboard/api-ref', '').replace('/dashboard/posts', '')
   return (
     <section className={styles.apiRefSection}>
-      {headerHost}
       <h1><AiOutlineCluster size='2.8rem' style={{marginBottom: '-0.5rem'}}/> API References</h1>
       <ApiRefCard method='GET' url={`${host}/api/posts`} description='Fetch all the published posts of the blog from newest to oldest'/>
+      <ApiRefCard method='GET' url={`${host}/api/posts?page={page}&limit={limit}`} description="Fetch posts with pagination, page's default is 1 and limit is 15"/>
       <ApiRefCard method='GET' url={`${host}/api/posts?order=oldest`} description='Fetch all the published posts of the blog starting from the older posts'/>
       <ApiRefCard method='GET' url={`${host}/api/posts?search={query}`} description='Search for posts using this endpoint with the search query parameter'/>
       <ApiRefCard method='GET' url={`${host}/api/posts?category={category}`} description='Search for posts based on a category'/>
