e734c04b8623e05726c68066869e4a20874f79fe
felbenini <felbenini@gmail.com>
2023-07-13 10:58:41 -0300

    Typed category model

--- lib/mongodb/models/Category.ts
+++ lib/mongodb/models/Category.ts
@@ -1,12 +1,24 @@
 import mongoose, { model } from 'mongoose'
 
-const CategorySchema = new mongoose.Schema({
+interface ICategory {
+  name: string,
+  type: string,
+  childCategories?: Array<mongoose.ObjectId> | Array<ICategory>,
+  mainCategory?: mongoose.ObjectId | ICategory
+}
+
+export interface ICategoryDocument extends ICategory, Document { }
+export interface ICategoryModel extends mongoose.Model<ICategoryDocument> {
+    buildUser(args: ICategory): ICategoryDocument
+}
+
+const CategorySchema: mongoose.Schema = new mongoose.Schema({
   name: {type: String, required: true},
   type: {type: String, required: true},
   childCategories: [{type: mongoose.Schema.Types.ObjectId, ref: 'Category'}],
   mainCategory: {type: mongoose.Schema.Types.ObjectId, ref: 'Category'}
 })
 
-const Category = mongoose.models.Category ||  model('Category', CategorySchema);
+const Category = mongoose.models.Category<ICategory> ||  model<ICategoryDocument, ICategoryModel>('Category', CategorySchema);
 
 export default Category
