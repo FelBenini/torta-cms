0f70c4acea38dd7956231c70124ace2dcd723fc9
felbenini <felbenini@gmail.com>
2023-07-18 13:13:54 -0300

    Turned the modal of adding new category into a component

--- app/dashboard/categories/Topbar.tsx
+++ app/dashboard/categories/Topbar.tsx
@@ -1,9 +1,8 @@
 'use client'
-import { Box, Button, Select, TextField, MenuItem, InputLabel, FormControl } from "@mui/material"
-import Modal from '@mui/material/Modal';
+import { Button } from "@mui/material"
 import { useState } from 'react'
-import styles from './style.module.scss'
 import { ICategory } from "@/lib/mongodb/models/Category";
+import NewCategoryModal from "@/components/Categories/NewCategoryModal";
 
 interface Category extends ICategory {
   _id: string
@@ -23,46 +22,14 @@ const style = {
 
 const Topbar = ({ length, categories }: { length: number, categories: Array<Category> }) => {
   const [open, setOpen] = useState(false)
-  const [mainCategory, setMainCategory] = useState(0)
   const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
-
-  const handleFormSubmit = (e: React.FormEvent) => {
-    e.preventDefault()
-  }
-
-  const handleSelectChange = (event: any) => {
-    setMainCategory(event.target.value);
-  };
   return (
     <>
       <span>
         <h3>Total: {length} {length === 1 ? 'category' : 'categories'}</h3>
         <Button onClick={handleOpen} variant='contained' sx={{ height: '48px' }}>New Category</Button>
       </span>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description">
-        <Box sx={style}>
-          <form className={styles.form} onSubmit={handleFormSubmit}>
-            <h2>Add New Category</h2>
-            <p>Main Category:</p>
-            <Select sx={{ width: '100%', marginTop: '12px' }} onChange={handleSelectChange} value={mainCategory}>
-              <MenuItem value={0}>None</MenuItem>
-              {categories.map((category, index) => (
-                <MenuItem value={category._id} key={index}>
-                  {category.name}
-                </MenuItem>
-              ))}
-            </Select>
-            <p>Choose your category&apos;s name:</p>
-            <TextField sx={{ width: '100%', marginTop: '12px' }} label='Category name' />
-            <Button variant="contained" sx={{ width: '100%', marginTop: '24px' }} type='submit'>Add Category</Button>
-          </form>
-        </Box>
-      </Modal>
+      <NewCategoryModal openState={open} setOpenState={setOpen} categories={categories} />
     </>
   )
 }
---
+++ components/Categories/NewCategoryModal.tsx
@@ -0,0 +1,64 @@
+import React, { SetStateAction, useState } from 'react';
+import { Button, Select, TextField, MenuItem } from "@mui/material"
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+import styles from './newcategory.module.scss'
+import { ICategory } from "@/lib/mongodb/models/Category";
+
+interface Category extends ICategory {
+  _id: string
+}
+
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  borderRadius: '8px',
+  boxShadow: 24,
+  p: 4,
+};
+
+const NewCategoryModal = ({ openState, setOpenState, categories }: { openState: boolean,
+  setOpenState: (value: SetStateAction<boolean>) => void,
+  categories: Array<Category>}) => {
+  const [mainCategory, setMainCategory] = useState(0)
+
+  const handleFormSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+  }
+
+  const handleSelectChange = (event: any) => {
+    setMainCategory(event.target.value);
+  };
+
+  return (
+    <Modal
+      open={openState}
+      onClose={() => setOpenState(false)}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description">
+      <Box sx={style}>
+        <form className={styles.form} onSubmit={handleFormSubmit}>
+          <h2>Add New Category</h2>
+          <p>Main Category:</p>
+          <Select sx={{ width: '100%', marginTop: '12px' }} onChange={handleSelectChange} value={mainCategory}>
+            <MenuItem value={0}>None</MenuItem>
+            {categories.map((category: Category, index: number) => (
+              <MenuItem value={category._id} key={index}>
+                {category.name}
+              </MenuItem>
+            ))}
+          </Select>
+          <p>Choose your category&apos;s name:</p>
+          <TextField sx={{ width: '100%', marginTop: '12px' }} label='Category name' />
+          <Button variant="contained" sx={{ width: '100%', marginTop: '24px' }} type='submit'>Add Category</Button>
+        </form>
+      </Box>
+    </Modal>
+  )
+}
+
+export default NewCategoryModal
