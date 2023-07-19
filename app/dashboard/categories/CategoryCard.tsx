ef9b5fff5d3e0b76bb51b959b9c7c1d413c64edf
felbenini <felbenini@gmail.com>
2023-07-18 21:12:53 -0300

    Changes on the category options

--- app/dashboard/categories/CategoryCard.tsx
+++ app/dashboard/categories/CategoryCard.tsx
@@ -9,19 +9,12 @@ import { IconButton } from '@mui/material'
 import { BiDotsVerticalRounded } from 'react-icons/bi'
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
+import { FaRegTrashAlt } from 'react-icons/fa'
 
 const CategoryCard = ({ id }: { id: string }) => {
   const pathname = useSearchParams()
   const [info, setInfo] = useState<{ name: string, childCategories: Array<ICategory> } | null>(null)
   const [loading, setLoading] = useState(true)
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
 
   useEffect(() => {
     console.log(pathname)
@@ -42,51 +35,52 @@ const CategoryCard = ({ id }: { id: string }) => {
       <h5>Name:</h5>
       <span className={styles.inline}>
         <h3>{info?.name}</h3>
-        <IconButton onClick={handleClick}
-          aria-controls={open ? 'basic-menu' : undefined}
-          aria-haspopup="true"
-          aria-expanded={open ? 'true' : undefined}>
-          <BiDotsVerticalRounded />
-        </IconButton>
-        <Menu
-          id="basic-menu"
-          anchorEl={anchorEl}
-          open={open}
-          onClose={handleClose}
-          MenuListProps={{
-            'aria-labelledby': 'basic-button',
-          }}
-        >
-          <MenuItem onClick={handleClose}>Delete Category</MenuItem>
-        </Menu>
+        <OptionsMenu name={info?.name as string} />
       </span>
       {info?.childCategories.length ? <h5>Child categories:</h5> : <></>}
       {info?.childCategories.map((category, index) => (
         <div className={styles.categoryCard} key={index}>
-          <h5>Category&apos;s name:</h5>
+          <h5>Subcategory&apos;s name:</h5>
           <span className={styles.inline}>
             <h3>{category.name}</h3>
-            <IconButton onClick={handleClick}
-              aria-controls={open ? 'basic-menu' : undefined}
-              aria-haspopup="true"
-              aria-expanded={open ? 'true' : undefined}>
-              <BiDotsVerticalRounded />
-            </IconButton>
-            <Menu
-              id="basic-menu"
-              anchorEl={anchorEl}
-              open={open}
-              onClose={handleClose}
-              MenuListProps={{
-                'aria-labelledby': 'basic-button',
-              }}
-            >
-              <MenuItem onClick={handleClose}>Delete Category</MenuItem>
-            </Menu>
+            <OptionsMenu name={category.name} />
           </span>
         </div>
       ))}
     </div>
+  )
+}
+
+function OptionsMenu({name}: {name: string}) {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <>
+      <IconButton onClick={handleClick}
+        aria-controls={open ? 'basic-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}>
+        <BiDotsVerticalRounded />
+      </IconButton>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuItem onClick={handleClose}><FaRegTrashAlt size={18} style={{marginRight: 6}}/> Delete {name} Category</MenuItem>
+      </Menu>
+    </>
   )
 }
 
