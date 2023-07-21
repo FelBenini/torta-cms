b52292b6220c7836b81e00d63a863fadfcb8d668
felbenini <felbenini@gmail.com>
2023-07-21 14:47:21 -0300

    Started to migrate from mongoose to prisma

---
+++ prisma/controllers/userController.ts
@@ -0,0 +1,54 @@
+import { PrismaClient, Prisma } from "@prisma/client"
+import { User } from "../types/User"
+import bcrypt from 'bcryptjs'
+
+const prisma = new PrismaClient({})
+
+function generateApiKey(length: number) {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return result;
+}
+
+export default class UserController {
+  public static getUserByName = async (username: string) => {
+    const user = await prisma.user.findFirst({where: {username: {equals: username, mode: 'insensitive'}}})
+    if (!user) {
+      return null
+    }
+    return user
+  }
+
+  public static createFirstUser = async (newUser: Prisma.UserCreateInput) => {
+    newUser.role = 'admin'
+    const encryptedPass = bcrypt.hashSync(newUser.password, bcrypt.genSaltSync(10))
+    newUser.apiKey = generateApiKey(105);
+    newUser.password = encryptedPass
+
+    const checkForUser = await prisma.user.findFirst()
+
+    if (checkForUser) {
+      throw new Error('Admin user already exists')
+      return
+    }
+
+    const user = await prisma.user.create({data: newUser})
+    return user
+  }
+
+  public static checkIfFirstUser = async () => {
+    const user = await prisma.user.findFirst()
+
+    if (user) {
+      return true
+    } else {
+      return false
+    }
+  }
+}
---
+++ prisma/types/User.ts
@@ -0,0 +1,10 @@
+export declare type User = {
+  id: string
+  username: string
+  password: string
+  email: string
+  role: string
+  updatedAt?: Date | number
+  profilePic?: string
+  apiKey?: string
+}
