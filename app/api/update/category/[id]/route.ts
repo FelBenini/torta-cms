207f453fa5108733df9af4e44b0b4f886347f004
felbenini <felbenini@gmail.com>
2023-07-28 17:28:57 -0300

    Merge conflict solved

--- app/api/update/category/[id]/route.ts
+++ app/api/update/category/[id]/route.ts
@@ -19,48 +19,16 @@ export async function PUT(req: NextRequest, { params }: { params: { id: string }
   if (!post) {
     return NextResponse.json({'message': 'Post does not exist'}, {status: 404})
   }
-<<<<<<< HEAD
-  if (post.categories?.includes(body.category)) {
-    const arrayCategories = post.categories.split(', ')
-    const updatedCategories = arrayCategories.filter((category) => category !== body.category)
-    const categories = updatedCategories.join(', ')
-    const newPost = await prisma.post.update({
-      where: {
-        id: id
-      },
-      data: {
-        categories: `${categories}, `
-      }
-    })
-    return NextResponse.json({ 'removed': newPost.categories })
-  } else {
-    let newCategories: string
-    if (post.categories === '' || post.categories === ' ' || post.categories === ', ' || !post.categories) {
-      newCategories = `${body.category}, `
-    } else {
-      newCategories = `${post.categories}${body.category}, `
-    }
-    const newPost = await prisma.post.update({
-      where: {
-        id: id
-      },
-      data: {
-        categories: newCategories
-      }
-    })
-    return NextResponse.json({ 'included': newPost.categories })
-  }
-=======
+
   const arrayCategories = body.categories as string[]
   const categories = arrayCategories.filter((cat) => cat !== '').join(', ')
   const newPost = await prisma.post.update({
     where: {
-      id: params.id
+      id: parseInt(params.id)
     },
     data: {
       categories: `${categories}, `
     }
   })
   return NextResponse.json({categories: newPost.categories})
->>>>>>> development
 }
